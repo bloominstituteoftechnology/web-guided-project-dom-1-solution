f185d427464a5cf15eb6fc4520732ae8259fd069
Gabriel Cabrejas <gabriel@cabrejas.net>
2020-07-06 16:07:07 +0200

    simplify

--- src/index.js
+++ src/index.js
@@ -1,124 +1,47 @@
-// See this in the console to make sure the project is wired up correctly
-console.log('The project is UP')
+// USING THE DOM TO EDIT ELEMENTS ON THE PAGE (AND ADD NEW ONES)
 
-// STEP 1- console.dir document and study it
+// 1- Finding an element on the page and saving a reference to it
+//  1a- querySelector
+//  1b- querySelectorAll
+const logoTitle = document.querySelector('#logoTitle')
 
-// STEP 2- Grab the Lambda Cat heading in the console. Access the following:
-//  - the .id
-//  - the .classList
-//  - the data inside a data-attribute using .dataset
-//  - the text content using .textContent
-//  - any attribute using .getAttribute('id') // example using id
+// 2- Changing an element's text content
+logoTitle.textContent = 'Lambda Dog'
 
-// STEP 3- find the body of the document in the DOM and find:
-//  - Its parentElement
-const parentOfBody = document.body.parentElement
-//  - Its firstElementChild
-const header = document.body.firstElementChild
-//  - Its firstElementChild's nextSibling element
-const cardContainer = document.body.firstElementChild.nextElementSibling
+// 3- Changing any property
+//  3a- with dot notation
+//  3b- setAttribute
+const image = document.querySelector('.card-group .card:nth-of-type(1) img')
+// image.setAttribute('src', 'https://herepup.com/wp-content/uploads/2015/12/Dog-Advice-1-1.jpg')
+image.src = 'https://herepup.com/wp-content/uploads/2015/12/Dog-Advice-1-1.jpg'
 
-// STEP 4- find the Lambda Cat heading with firstElementChild and nextSibling
-const heading = document.body.firstElementChild.firstElementChild.firstElementChild.nextElementSibling
-console.log(heading, '<- barbara')
+// 4- Changing the style
+//  4a- by changing class names on the element
+const header = document.querySelector('header')
+header.classList.add('sky')
+header.classList.remove('sky')
 
-// STEP 5- find the children of the nav, convert it into a real array
-const navChildren = document.body.firstElementChild.firstElementChild.nextElementSibling.children
-console.log(navChildren, '<- gabri')
+setInterval(() => {
+  header.classList.toggle('sky')
+}, 1000)
 
-const navChildrenArr = Array.from(navChildren)
-console.log(navChildrenArr, '<- the REAL array')
+//  4b- by manipulating inline styles on the element
+header.style.background = 'pink'
 
-navChildrenArr.forEach(link => {
-  console.log(link.textContent)
-})
-
-// STEP 6- NO CSS SELECTOR - find the following elements or groups of elements:
-//  - all the divs in the page
-//  - all elements with a className of 'card'
-//  - the element with an id of 'mainNav'
-document.getElementsByTagName('div')
-document.getElementsByClassName('card')
-document.getElementById('mainNav')
-
-// examples
-const allLinks = document.getElementsByTagName('a') // [COLLECTION]
-const allCards = document.getElementsByClassName('card') // [COLLECTION]
-const logoTitleOld = document.getElementById('logo-title') // [SINGLE ELEMENT]
-
-// STEP 7- do the same, but using CSS selectors
-//  - all the divs in the page
-//  - all elements with a className of 'card'
-//  - the element with an id of 'mainNav'
-
-document.querySelectorAll('div')
-document.querySelectorAll('.card')
-document.querySelector('#mainNav')
-
-// examples
-const logoTitle = document.querySelector('#logo-title') // [SINGLE ELEMENT]
-const anchorTagsNav = document.querySelectorAll('nav a') // [COLLECTION]
-
-const nav = document.querySelector('nav') // [SINGLE ELEMENT]
-const linksfromNav = nav.querySelectorAll('a') // [COLLECTION]
-
-// STEP 8- use setAttribute to change an attribute of an element
-// elem.getAttribute('href')
-// elem.setAttribute('id', 'newId')
-
-// (change the href of the first link inside the nav
-// so it takes to google.com)
-
-// 1- capture the element
-// 2- set the attribute
-nav.firstElementChild.setAttribute('href', 'https://google.com') 
-
-// STEP 9- use style to change inline styles of an element
-//  - make the first h3 subheading be a different color
-const theH3 = document.querySelector('h3')
-theH3.style.color = 'green'
-//  - make all the h3 subheadings be a different color using a loop
-const theH3s = document.querySelectorAll('h3')
-theH3s.forEach(link => link.style.color = 'orange')
-
-// STEP 10- use classList to add, remove and toggle a class name
-// elem.classList // the list of classNames
-// elem.classList.add('foo') // this adds a className of 'foo' to elem
-// elem.classList.remove('foo') // this removes the className of 'foo' from elem
-// elem.classList.toggle('foo') // this toggles the className on the element
-
-//  - remove the 'extra-stuff' class name from the last paragraph
-//  - add it to the first paragraph
-//  - toggle it in the second paragraph
-document.querySelectorAll('.extra-stuff').forEach(elem => elem.classList.toggle('extra-stuff'))
-
-// STEP 11- get a collection of h2s and loop over them setting text content from cardTitles
-const cardTitles = {
-  cardTitle1: 'Cats Are the Supreme Rulers',
-  cardTitle2: 'Cats Rule Dogs Drool',
-  cardTitle3: 'Give Me All the Belly Rubs',
-  cardTitle4: 'I Haz CheeseBurger',
-}
-const keys = Object.keys(cardTitles)
-
-document.querySelectorAll('h2').forEach((h2, idx) => {
-  h2.textContent = cardTitles[keys[idx]]
-})
-
-// STEP 12- create a new link and append it to the main nav
-const newLinkProps = {
-  href: '#',
-  textContent: 'Site Map',
-  className: 'menu-item',
-}
-
-// create link out of thin air, put it in a variable
+// 5- Creating new elements and appending them
 const newLink = document.createElement('a')
+newLink.textContent = 'Blog'
+newLink.classList.add('menu-item')
+newLink.href = '#'
+document.querySelector('nav').appendChild(newLink)
 
-// add all the attrs and content it needs
-newLink.href = newLinkProps.href
-newLink.textContent = newLinkProps.textContent
-newLink.classList.add(newLinkProps.className)
+// 6- Removing an existing element and putting it back
+header.remove()
+const body = document.body
+body.prepend(header)
 
-// append it to the nav
-nav.appendChild(newLink)
+// 7- Making a copy of the card and appending it to the card group
+const card = document.querySelector('.card-group .card:nth-of-type(1)')
+const copyOfCard = card.cloneNode(true)
+const cardGroup = document.querySelector('.card-group')
+cardGroup.appendChild(copyOfCard)
