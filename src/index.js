b2493d8ec6bc5d5eed219fc7104b09b1e0caf3fc
Gabriel Cabrejas <gabriel@cabrejas.net>
2020-08-03 15:46:25 +0200

    solution

--- src/index.js
+++ src/index.js
@@ -4,46 +4,52 @@
 //  getElementById, getElementsByTagName, getElementsByClassName
 //  querySelector
 //  querySelectorAll
+const header = document.querySelector('header')
 const logoTitle = document.querySelector('#logoTitle')
+const firstCard = document.querySelector('.card:nth-of-type(1)')
+const imageFirstCard = firstCard.querySelector('img')
+const titleFirstCard = firstCard.querySelector('h2')
+const subtitleFirstCard = firstCard.querySelector('h3')
+const textFirstCard = firstCard.querySelector('p')
+const link1FirstCard = textFirstCard.nextElementSibling
+const link2FirstCard = link1FirstCard.nextElementSibling
 
 // 2- Changing an element's text content
 //  textContent, innerText
 logoTitle.textContent = 'Lambda Dog'
+titleFirstCard.textContent = 'I rulez'
+subtitleFirstCard.textContent = 'obviously'
+textFirstCard.textContent = 'Puggorino tungg puggorino, waggy wags. thicc wow such tempt. Very jealous pupper doing me a frighten puggorino very taste wow, borkdrive. Smol big ol fluffer pupperino fluffer, waggy wags ruff blep.'
+link2FirstCard.textContent = 'Dog Ipsum'
 
 // 3- Changing any property
 //  with dot notation
+logoTitle.className = 'logo heading banana'
+imageFirstCard.src = 'https://herepup.com/wp-content/uploads/2015/12/Dog-Advice-1-1.jpg'
 //  setAttribute, getAttribute
-const image = document.querySelector('.card-group .card:nth-of-type(1) img')
-// image.setAttribute('src', 'https://herepup.com/wp-content/uploads/2015/12/Dog-Advice-1-1.jpg')
-image.src = 'https://herepup.com/wp-content/uploads/2015/12/Dog-Advice-1-1.jpg'
+link1FirstCard.setAttribute('href', 'https://www.pexels.com/search/dog/')
+link2FirstCard.setAttribute('href', 'https://doggoipsum.com/')
 
 // 4- Changing the style
 //  by changing the class names on the element
 //  by manipulating inline styles on the element
-const header = document.querySelector('header')
 header.classList.add('sky')
 header.classList.remove('sky')
 
 setInterval(() => {
   header.classList.toggle('sky')
-}, 1000)
-
-header.style.background = 'pink'
+}, 1000);
 
 // 5- Creating new elements and appending them
-const newLink = document.createElement('a')
-newLink.textContent = 'Blog'
-newLink.classList.add('menu-item')
-newLink.href = '#'
-document.querySelector('nav').appendChild(newLink)
-
+const blogLink = document.createElement('a')
+blogLink.textContent = 'Blog'
+blogLink.href = '#'
+document.querySelector('nav').appendChild(blogLink)
 // 6- Removing an existing element and putting it back
 header.remove()
-const body = document.body
-body.prepend(header)
+document.body.prepend(header)
 
 // 7- Making a copy of the card and appending it to the card group
-const card = document.querySelector('.card-group .card:nth-of-type(1)')
-const copyOfCard = card.cloneNode(true)
-const cardGroup = document.querySelector('.card-group')
-cardGroup.appendChild(copyOfCard)
+const secondCard = firstCard.cloneNode(true)
+document.querySelector('.card-group')
+  .appendChild(secondCard)
